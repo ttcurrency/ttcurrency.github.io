5759c1c6328824819652dbb2ce0225686823a9d1
weitat <weitat@curlec.com>
2020-08-24 00:33:03 +0800

    update color

--- js/main.js
+++ js/main.js
@@ -311,4 +311,24 @@
       });
     });
 
+
+    $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:20,
+      dots:true,
+      autoplay:true,
+      autoplayTimeout:5000,
+      autoplayHoverPause:false,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:1
+          },
+          1000:{
+              items:1
+          }
+      }
+  })
 })(jQuery);
